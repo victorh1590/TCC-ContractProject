21354d05d86945c103e47498a7de399778ea61ce
Victor H F <51718734+victorh1590@users.noreply.github.com>
2023-05-14 22:15:23 -0300

    Migrates from REST to GRPC.

--- Voting.Server/Program.cs
+++ Voting.Server/Program.cs
@@ -1,28 +1,50 @@
 using Voting.Server.Persistence.Accounts;
+using Voting.Server.Services;
 
+//GRPC
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Add services to the container.
+builder.Services.AddGrpc();
 builder.Services.AddSingleton<IAccountManager, AccountManager>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.MapGrpcService<VotingService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
+
+//Rest
+//
+//
+// var builder = WebApplication.CreateBuilder(args);
+//
+// // Add services to the container.
+//
+// builder.Services.AddControllers();
+// // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
+// builder.Services.AddSingleton<IAccountManager, AccountManager>();
+//
+// var app = builder.Build();
+//
+// // Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+//
+// app.UseHttpsRedirection();
+//
+// app.UseAuthorization();
+//
+// app.MapControllers();
+//
+// app.Run();
---
+++ Voting.Server/Services/VotingService.cs
@@ -0,0 +1,72 @@
+﻿using Grpc.Core;
+using Voting.Server.Protos;
+using Google.Protobuf.WellKnownTypes;
+using Voting.Server.Domain;
+using Voting.Server.Domain.Models;
+using static Voting.Server.Protos.VotingService;
+
+namespace Voting.Server.Services
+{
+    public class VotingService : VotingServiceBase
+    {
+        private readonly DomainService _domainService;
+
+
+        internal VotingService(DomainService domainService)
+        {
+            _domainService = domainService;
+        }
+
+        //[global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
+        public override async Task<SectionListReply> GetCandidateVotes(CandidateIdRequest request, ServerCallContext context)
+        {
+            List<Section> sections = await _domainService.GetVotesByCandidateAsync(request.Candidate);
+            SectionListReply reply = new SectionListReply();
+            foreach(Section section in sections)
+            {
+                List<Protos.CandidateVotes> cvList = new List<Protos.CandidateVotes>();
+                foreach(Domain.Models.CandidateVotes cv in section.CandidateVotes)
+                {
+                    Protos.CandidateVotes cvReply = new Protos.CandidateVotes
+                    {
+                        Candidate = cv.Candidate,
+                        Votes = cv.Votes
+                    };
+                    cvList.Add(cvReply);
+                }
+
+                SectionReply sectionReply = new SectionReply
+                {
+                    SectionID = section.SectionID
+                };
+                sectionReply.CandidateVotes.AddRange(cvList);
+                reply.Sections.Add(sectionReply);
+            }
+            return await Task.FromResult(reply);
+        }
+
+        //[global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
+        public override async Task<SectionReply> GetSectionVotes(SectionIdRequest request, ServerCallContext context)
+        {
+            throw new RpcException(new Status(StatusCode.Unimplemented, ""));
+        }
+
+        //[global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
+        public override async Task<TotalVotesReply> GetTotalVotes(Empty request, ServerCallContext context)
+        {
+            throw new RpcException(new Status(StatusCode.Unimplemented, ""));
+        }
+
+        //[global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
+        public override async Task<TotalVotesReply> GetTotalVotesByCandidate(CandidateIdRequest request, ServerCallContext context)
+        {
+            throw new RpcException(new Status(StatusCode.Unimplemented, ""));
+        }
+
+        //[global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
+        public override async Task CreateSection(IAsyncStreamReader<SectionsCreationRequest> requestStream, IServerStreamWriter<SectionsCreationReply> responseStream, ServerCallContext context)
+        {
+            throw new RpcException(new Status(StatusCode.Unimplemented, ""));
+        }
+    }
+}
