1db4ae9f7ec17a46d27d555a5bfc8fb000d12188
Victor H F <51718734+victorh1590@users.noreply.github.com>
2023-05-14 23:39:20 -0300

    Add service methods.

--- Voting.Server/Services/VotingService.cs
+++ Voting.Server/Services/VotingService.cs
@@ -3,70 +3,109 @@ using Voting.Server.Protos;
 using Google.Protobuf.WellKnownTypes;
 using Voting.Server.Domain;
 using Voting.Server.Domain.Models;
+using System.Linq;
+using CommunityToolkit.Diagnostics;
 using static Voting.Server.Protos.VotingService;
+using CandidateVotes = Voting.Server.Domain.Models.CandidateVotes;
 
-namespace Voting.Server.Services
+namespace Voting.Server.Services;
+
+public class VotingService : VotingServiceBase
 {
-    public class VotingService : VotingServiceBase
+    private readonly DomainService _domainService;
+
+
+    internal VotingService(DomainService domainService)
     {
-        private readonly DomainService _domainService;
+        _domainService = domainService;
+    }
 
-
-        internal VotingService(DomainService domainService)
+    //[global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
+    public override async Task<SectionListReply> GetCandidateVotes(CandidateIdRequest request, ServerCallContext context)
+    {
+        List<Section> sections = await _domainService.GetVotesByCandidateAsync(request.Candidate);
+        SectionListReply reply = new SectionListReply();
+        foreach(Section section in sections)
         {
-            _domainService = domainService;
-        }
-
-        //[global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
-        public override async Task<SectionListReply> GetCandidateVotes(CandidateIdRequest request, ServerCallContext context)
-        {
-            List<Section> sections = await _domainService.GetVotesByCandidateAsync(request.Candidate);
-            SectionListReply reply = new SectionListReply();
-            foreach(Section section in sections)
+            List<Protos.CandidateVotes> cvList = new List<Protos.CandidateVotes>();
+            foreach(Domain.Models.CandidateVotes cv in section.CandidateVotes)
             {
-                List<Protos.CandidateVotes> cvList = new List<Protos.CandidateVotes>();
-                foreach(Domain.Models.CandidateVotes cv in section.CandidateVotes)
+                Protos.CandidateVotes cvReply = new Protos.CandidateVotes
                 {
-                    Protos.CandidateVotes cvReply = new Protos.CandidateVotes
-                    {
-                        Candidate = cv.Candidate,
-                        Votes = cv.Votes
-                    };
-                    cvList.Add(cvReply);
-                }
-
-                SectionReply sectionReply = new SectionReply
-                {
-                    SectionID = section.SectionID
+                    Candidate = cv.Candidate,
+                    Votes = cv.Votes
                 };
-                sectionReply.CandidateVotes.AddRange(cvList);
-                reply.Sections.Add(sectionReply);
+                cvList.Add(cvReply);
             }
-            return await Task.FromResult(reply);
+
+            SectionReply sectionReply = new SectionReply
+            {
+                SectionID = section.SectionID
+            };
+            sectionReply.CandidateVotes.AddRange(cvList);
+            reply.Sections.Add(sectionReply);
+        }
+        return await Task.FromResult(reply);
+    }
+
+    //[global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
+    public override async Task<SectionReply> GetSectionVotes(SectionIdRequest request, ServerCallContext context)
+    {
+        Section section = await _domainService.GetSectionAsync(request.Section);
+        List<Protos.CandidateVotes> cvList = new List<Protos.CandidateVotes>();
+        foreach(Domain.Models.CandidateVotes cv in section.CandidateVotes)
+        {
+            Protos.CandidateVotes cvReply = new Protos.CandidateVotes
+            {
+                Candidate = cv.Candidate,
+                Votes = cv.Votes
+            };
+            cvList.Add(cvReply);
         }
 
-        //[global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
-        public override async Task<SectionReply> GetSectionVotes(SectionIdRequest request, ServerCallContext context)
+        SectionReply sectionReply = new SectionReply
         {
-            throw new RpcException(new Status(StatusCode.Unimplemented, ""));
-        }
+            SectionID = section.SectionID
+        };
+        sectionReply.CandidateVotes.AddRange(cvList);
 
-        //[global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
-        public override async Task<TotalVotesReply> GetTotalVotes(Empty request, ServerCallContext context)
-        {
-            throw new RpcException(new Status(StatusCode.Unimplemented, ""));
-        }
+        return await Task.FromResult(sectionReply);
+    }
 
-        //[global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
-        public override async Task<TotalVotesReply> GetTotalVotesByCandidate(CandidateIdRequest request, ServerCallContext context)
-        {
-            throw new RpcException(new Status(StatusCode.Unimplemented, ""));
-        }
+    //[global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
+    public override async Task<TotalVotesReply> GetTotalVotesBySection(SectionIdRequest request, ServerCallContext context)
+    {
+        long totalVotes = await _domainService.GetTotalVotesBySectionAsync(request.Section);
+        return new TotalVotesReply { TotalVotes = totalVotes };
+    }
 
-        //[global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
-        public override async Task CreateSection(IAsyncStreamReader<SectionsCreationRequest> requestStream, IServerStreamWriter<SectionsCreationReply> responseStream, ServerCallContext context)
+    //[global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
+    public override async Task<TotalVotesReply> GetTotalVotesByCandidate(CandidateIdRequest request, ServerCallContext context)
+    {
+        long totalVotes = await _domainService.GetTotalVotesByCandidateAsync(request.Candidate);
+        return new TotalVotesReply { TotalVotes = totalVotes };
+    }
+
+    //[global::System.CodeDom.Compiler.GeneratedCode("grpc_csharp_plugin", null)]
+    public override async Task CreateSection(IAsyncStreamReader<SectionCreationRequest> requestStream, IServerStreamWriter<SectionsCreationReply> responseStream, ServerCallContext context)
+    {
+        await foreach (SectionCreationRequest sectionCreationRequest in requestStream.ReadAllAsync())
         {
-            throw new RpcException(new Status(StatusCode.Unimplemented, ""));
+            List<Section> sectionList = new();
+            List<CandidateVotes> cv = new();
+            foreach (var candidateVotes in sectionCreationRequest.Section.CandidateVotes)
+            {
+                Guard.IsNotNull(candidateVotes);
+                cv.Add(new CandidateVotes
+                {
+                    Candidate = candidateVotes.Candidate,
+                    Votes = candidateVotes.Votes
+                });
+            }
+            Section section = new Section(sectionCreationRequest.Section.SectionID, cv);
+
+            sectionList.Add(section);
+            await _domainService.InsertSectionsAsync(sectionList);
         }
     }
 }
