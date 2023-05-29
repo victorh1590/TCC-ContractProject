bda91d50d24e645263d51abd70fd2256981fa3b0
Victor H F <51718734+victorh1590@users.noreply.github.com>
2023-05-29 12:42:28 -0300

    Split commands and options in classes. Fixes error handling.

---
+++ Voting.Client/ConsoleCommands.cs
@@ -0,0 +1,117 @@
+﻿using System.CommandLine;
+using System.Linq;
+
+namespace Voting.Client;
+
+public static class ConsoleCommands
+{
+    public static RootCommand RootCommand { get; }
+    public static Command GetCommand { get; }
+    public static Command AddCommand { get; }
+    public static Command TotalCommand { get; }
+
+    static ConsoleCommands()
+    {
+        //Create commands.
+        //Root command.
+        RootCommand = new RootCommand("Sample app for System.CommandLine");
+
+        //Subcommands
+        GetCommand = new Command("get", "Get voting data from blockchain.")
+        {
+            ConsoleOptions.SectionOption,
+            ConsoleOptions.CandidateOption
+        };
+        GetCommand.AddValidator(result =>
+        {
+            if (result.Tokens.Count == 0 || result.ErrorMessage?.Length >= 1)
+            {
+                result.ErrorMessage = "Must set a value for option --section, --candidate or both.";
+            }
+        });
+
+        TotalCommand = new Command("total", "Get totalization of votes from blockchain.")
+        {
+            ConsoleOptions.SectionOption,
+            ConsoleOptions.CandidateOption
+        };
+        TotalCommand.AddValidator(result =>
+        {
+            if (result.Tokens.Count == 0 || result.ErrorMessage?.Length >= 1)
+            {
+                result.ErrorMessage = "Must set a value for option --section, --candidate or both.";
+            }
+        });
+
+        AddCommand = new Command("add", "Send voting data to the blockchain.")
+        {
+            ConsoleOptions.JsonOption
+        };
+        AddCommand.AddValidator(result =>
+        {
+            if (result.Tokens.Count == 0 || result.ErrorMessage?.Length >= 1)
+            {
+                result.ErrorMessage = "Must set a value for --json or --csv.";
+            }
+        });
+        AddCommand.AddAlias("insert");
+        AddCommand.AddAlias("send");
+        AddCommand.AddAlias("put");
+
+        RootCommand.AddCommand(GetCommand);
+        RootCommand.AddCommand(TotalCommand);
+        RootCommand.AddCommand(AddCommand);
+
+        //Handlers.
+        GetCommand.SetHandler(
+            async (section, candidate) =>
+            {
+                var client = new GrpcClient();
+                if (section != null && candidate != null)
+                {
+                    throw new NotImplementedException("Not implemented");
+                }
+                else if (section != null)
+                {
+                    // await ReadFile(file!, delay, fgcolor, lightMode);
+                    await client.GetSectionVotes((uint)section);
+                }
+                else if (candidate != null)
+                {
+                    await client.GetCandidateVotes((uint)candidate);
+                }
+                else
+                {
+                    throw new Exception("No suitable method.");
+                }
+
+                client.Dispose();
+            },
+            ConsoleOptions.SectionOption, ConsoleOptions.CandidateOption);
+
+        TotalCommand.SetHandler(
+            async (section, candidate) =>
+            {
+                var client = new GrpcClient();
+                if (section != null && candidate != null)
+                {
+                    throw new NotImplementedException("Not implemented");
+                }
+                else if (section != null)
+                {
+                    await client.GetTotalVotesBySection((uint)section);
+                }
+                else if (candidate != null)
+                {
+                    await client.GetTotalVotesByCandidate((uint)candidate);
+                }
+                else
+                {
+                    throw new Exception("No suitable method.");
+                }
+
+                client.Dispose();
+            },
+            ConsoleOptions.SectionOption, ConsoleOptions.CandidateOption);
+    }
+}
---
+++ Voting.Client/ConsoleOptions.cs
@@ -0,0 +1,90 @@
+﻿using System.CommandLine;
+
+namespace Voting.Client;
+
+public static class ConsoleOptions
+{
+    public static Option<FileInfo?> JsonOption { get; }
+    public static Option<uint?> SectionOption { get; }
+    public static Option<uint?> CandidateOption { get; }
+
+    static ConsoleOptions()
+    {
+        //Options(flags)
+        JsonOption = new Option<FileInfo?>(
+            name: "--json",
+            description: "An option whose argument json file is parsed as a FileInfo.",
+            isDefault: true, //Default value exists.
+            parseArgument: result => // Validation.
+            {
+                if (!result.Tokens.Any())
+                {
+                    return null; // Default.
+                }
+
+                string? filePath = result.Tokens.SingleOrDefault()?.Value;
+                FileInfo fi = new FileInfo(filePath ?? string.Empty);
+                if (String.IsNullOrEmpty(fi.FullName) || !fi.Exists)
+                {
+                    result.ErrorMessage = "File does not exist.";
+                    return null;
+                }
+
+                if (fi.Extension != "json")
+                {
+                    result.ErrorMessage = "File is not json.";
+                    return null;
+                }
+
+                return fi;
+            });
+
+        SectionOption = new Option<uint?>(
+            name: "--section",
+            description: "Sections to look for when querying the blockchain.",
+            isDefault: true, //Default value exists.
+            parseArgument: result => // Validation.
+            {
+                if (!result.Tokens.Any())
+                {
+                    return null; // Default.
+                }
+
+                string sectionString = result.Tokens.SingleOrDefault()?.Value ?? string.Empty;
+                if (!uint.TryParse(sectionString, out uint section))
+                {
+                    result.ErrorMessage = result.LocalizationResources.ArgumentConversionCannotParse("SectionNumber", typeof(uint));
+                    return null;
+                }
+
+                return section;
+            });
+
+        CandidateOption = new Option<uint?>(
+            name: "--candidate",
+            description: "Candidate to look for when querying the blockchain.",
+            isDefault: true, //Default value exists.
+            parseArgument: result => // Validation.
+            {
+                // if (result.Tokens.Count == 0)
+                // {
+                //     result.ErrorMessage = "No candidate passed.";
+                // }
+                
+                if (result.Tokens.Count == 0)
+                {
+                    return null; // Default.
+                }
+
+                string candidateString = result.Tokens.SingleOrDefault()?.Value ?? string.Empty;
+                if (!uint.TryParse(candidateString, out uint candidate))
+                {
+                    // result.ErrorMessage = "Invalid candidate. Argument must be uint.";
+                    result.ErrorMessage = result.LocalizationResources.ArgumentConversionCannotParse("CandidateNumber", typeof(uint));
+                    return null;
+                }
+
+                return candidate; // Default
+            });
+    }
+}
